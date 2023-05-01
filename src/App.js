6e291c0644dbebe3a489e58944b556e75c74fa36
Jake <jake@toomay.dev>
2023-04-30 19:51:11 -0500

    Simple Project Cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import tw, { styled } from 'twin.macro';
+import './App.css';
 import { useState } from 'react';
 
 function App() {
@@ -11,11 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
           <Test toggle={toggle}>Test Component</Test>
           <TestButton onClick={() => setToggle(!toggle)}>Click me to test styledComponents</TestButton>
           Learn React
